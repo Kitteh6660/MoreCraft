a5aa64d651b7838735866f9cc71b9db12ac2c47a
Kitteh6660 <sylvainkittens@gmail.com>
2017-05-30 21:52:06 -0400

    Version correction
    Didn't compile correctly but now it should.

--- kittehmod/bettercraft/MoreCraft.java
+++ kittehmod/bettercraft/MoreCraft.java
@@ -29,11 +29,7 @@ import net.minecraftforge.oredict.OreDictionary;
 public class MoreCraft 
 {
     public static final String MODID = "bettercraft";
-<<<<<<< HEAD
-    public static final String VERSION = "3.0.1";
-=======
-    public static final String VERSION = "2.9.3";
->>>>>>> d58c1f6... MoreCraft 2.9.3 for Minecraft 1.9.4
+    public static final String VERSION = "3.0.3";
 	
 	// The instance of your mod that Forge uses.
 	@Instance("bettercraft")
